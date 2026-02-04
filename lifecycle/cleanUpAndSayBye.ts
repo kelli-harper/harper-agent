6e123a7898f61df7df68cde50ae8740380159e7c
Dawson Toth <dawson@harperdb.io>
2026-02-04 11:05:14 -0500

    fix: Force flush traces before exiting

--- lifecycle/cleanUpAndSayBye.ts
+++ lifecycle/cleanUpAndSayBye.ts
@@ -1,3 +1,4 @@
+import { getGlobalTraceProvider } from '@openai/agents';
 import { costTracker } from '../utils/sessions/cost';
 import { harperProcess } from '../utils/shell/harperProcess';
 import { harperResponse } from '../utils/shell/harperResponse';
@@ -11,4 +12,5 @@ export async function cleanUpAndSayBye() {
 	}
 	console.log('');
 	harperResponse('See you later!');
+	await getGlobalTraceProvider().forceFlush();
 }
